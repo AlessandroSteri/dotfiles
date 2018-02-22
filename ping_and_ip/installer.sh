1ebc98a77866e24016817f3115abb7f489d2ef2a
Alessandro Steri <alessandro.steri@gmail.com>
2018-02-22 12:16:25 +0100

    add whoami and host to ping_ip

--- ping_and_ip/installer.sh
+++ ping_and_ip/installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 pip3 install telegram-send
+echo "Follow instructions:"
 telegram-send --configure
+echo "Test it:"
 telegram-send "hello, world"
--- ping_and_ip/startup.sh
+++ ping_and_ip/startup.sh
@@ -17,15 +17,18 @@
 #
 while [ 1 ]
 do
-    sleep 2m
-    echo "running" > ~/Desktop/looooog.txt
+    # sleep 2m #maybe obsolete
+    # echo "running" > ~/Desktop/startup_log.txt
     TMP_PING=$(curl https://ping.eu | grep "Your IP is")
     PING=${TMP_PING:14:15}
     IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
-
+    WHO=$(whoami)
+    HOST=$(hostname)
+    # check if differend and send only if it is
+    telegram-send "WHOAMI: $WHO, HOST: $HOST"
     telegram-send $PING
     telegram-send $IP
-    sleep 1800
-    # sleep 30m
+    # echo $(date +%H:%M:%S)
+    sleep 3600
 done
 # telegram-send "PING: $PING,  IP: $IP"
