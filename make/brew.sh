ce8395309c976bee3dcafc63667dd41f004d4177
Alessandro Steri <alessandro.steri@gmail.com>
2018-03-06 22:46:44 +0100

    remove double brew install

--- make/brew.sh
+++ make/brew.sh
@@ -1,11 +1,5 @@
 #!bin/bash
 
-# Check for Homebrew, install if we don't have it
-if test ! $(which brew); then
-    echo "Installing homebrew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 # Update homebrew recipes
 brew update
 
