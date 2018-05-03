9072e43205d4b144db50f3f6723cdc40a2115d3a
Alessandro Steri <alessandro.steri@gmail.com>
2018-05-03 20:40:46 +0200

    update post install instructios

--- utils/ununtu_post_install.sh
+++ utils/ununtu_post_install.sh
@@ -3,3 +3,8 @@
 #download latest version
 #cd to download folder
 #bash Anaconda-latestversion-Linux-x86_64.sh
+
+
+#tensorflow gpu https://github.com/markjay4k/Install-Tensorflow-on-Ubuntu-17.10-/blob/master/Tensorflow%20Install%20instructions.ipynb
+
+#todo migliora fonts, torna a sauce code pro nerd font che better tmux over ssh
