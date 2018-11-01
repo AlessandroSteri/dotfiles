ae5d6025c2b7460cbb26fb9943c6b49330361cb7
Alessandro Steri <steri.1356261@studenti.uniroma1.it>
2018-11-02 00:18:36 +0100

    Update zsh.sh

--- make/zsh.sh
+++ make/zsh.sh
@@ -5,7 +5,7 @@ if [ ! $(command_exists "zsh") ]; then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel9k]; then
+if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel9k ]; then
     echo "[ITERM2][Oh My Zsh][powerlevel9k] Installing..."
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
